2ad95ade64536b8ec6d7fe34726ba8f9859be959
Andrei Bogdan <andrei.bogdan.mail@gmail.com>
2016-03-07 18:12:30 +0200

    intialized the map & center the map

--- FourPaneDemo/FourPaneDemo/ViewController.m
+++ FourPaneDemo/FourPaneDemo/ViewController.m
@@ -6,9 +6,12 @@
 //  Copyright © 2016 Andrei Bogdan. All rights reserved.
 //
 
+#import "MapKit/MapKit.h"
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
 
 @end
 
@@ -17,11 +20,35 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //initialize web page
+    NSString *webURL = @"https://www.facebook.com/";
+    
+    NSMutableURLRequest *request =  [NSMutableURLRequest requestWithURL: [NSURL URLWithString:webURL]];
+    
+    [self.webView loadRequest:request];
+    
+    //center the map
+    double latitude = 34.448795;
+    double longitude = -119.6646337;
+    
+    MKPointAnnotation *wiclAnno = [[MKPointAnnotation alloc] init];
+    wiclAnno.coordinate = CLLocationCoordinate2DMake(latitude, longitude);
+    wiclAnno.title = @"Testing";
+    
+    [self.mapView addAnnotation:wiclAnno];
+    
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(wiclAnno.coordinate, 10000, 10000);
+    MKCoordinateRegion adjusted = [self.mapView regionThatFits:region];
+    [self.mapView setRegion:adjusted animated:YES];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
 
 @end
